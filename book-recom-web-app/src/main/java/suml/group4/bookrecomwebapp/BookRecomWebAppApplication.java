4c6c817e4eadfb4963f453049da70d56214a1ad8
Sasha Kurchak <skurchak46@icloud.com>
2023-11-20 13:31:28 +0100

    Initialized Java Spring project

---
+++ book-recom-web-app/src/main/java/suml/group4/bookrecomwebapp/BookRecomWebAppApplication.java
@@ -0,0 +1,13 @@
+package suml.group4.bookrecomwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecomWebAppApplication.class, args);
+	}
+
+}
---
+++ book-recom-web-app/src/test/java/suml/group4/bookrecomwebapp/BookRecomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package suml.group4.bookrecomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

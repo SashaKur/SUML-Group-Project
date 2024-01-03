c284524fcf80b85187fe99f3a29d405dcf5e2684
Polina <lemann676@gmail.com>
2024-01-03 20:04:50 +0100

    add tensor flow model

--- book-recommendation-ml/src/book_recommendation_ml/pipeline_registry.py
+++ book-recommendation-ml/src/book_recommendation_ml/pipeline_registry.py
@@ -3,8 +3,14 @@ from __future__ import annotations
 
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-from .pipelines.model_training.pipeline import create_model_training_pipeline
-from .pipelines.data_preprocessing.pipeline import create_data_preprocessing_pipeline
+from .pipelines.model_training import regressor_pipeline
+from .pipelines.model_training import tensorflow_pipeline
+from .pipelines.data_preprocessing.standard_preprocessing_pipeline import (
+    create_data_preprocessing_pipeline,
+)
+from .pipelines.data_preprocessing.tensorflow_prep_pipeline import (
+    create_tensor_flow_prep_pipeline,
+)
 
 
 def register_pipelines() -> dict[str, Pipeline]:
@@ -15,10 +21,14 @@ def register_pipelines() -> dict[str, Pipeline]:
     """
     pipelines = find_pipelines()
     data_processing_pipeline = create_data_preprocessing_pipeline()
-    model_train_pipeline = create_model_training_pipeline()
+    model_train_pipeline = regressor_pipeline.create_model_training_pipeline()
+    tensorflow_prep_pipeline = create_tensor_flow_prep_pipeline()
+    tensorflow_train_pipeline = tensorflow_pipeline.create_model_training_pipeline()
 
     return {
         "__default__": model_train_pipeline,
-        "dp": data_processing_pipeline,
-        "mt": model_train_pipeline,
+        "data_processing": data_processing_pipeline,
+        "model_train": model_train_pipeline,
+        "tf_prep": tensorflow_prep_pipeline,
+        "tf_train": tensorflow_train_pipeline,
     }
--- book-recommendation-ml/src/book_recommendation_ml/pipelines/data_preprocessing/nodes.py
+++ book-recommendation-ml/src/book_recommendation_ml/pipelines/data_preprocessing/nodes.py
@@ -41,10 +41,12 @@ def drop_unnecessary_columns(df: pd.DataFrame) -> pd.DataFrame:
         "Image-URL-S",
         "Image-URL-M",
         "Image-URL-L",
-        "Book-Title",
+        "Book-Author",
         "Publisher",
+        "Year-Of-Publication",
     ]
-    return df.drop(columns=columns_to_drop)
+    df.drop(columns_to_drop, axis=1, inplace=True)
+    return df
 
 
 def make_isbn_numeric(df: pd.DataFrame) -> pd.DataFrame:
@@ -57,3 +59,59 @@ def make_isbn_numeric(df: pd.DataFrame) -> pd.DataFrame:
     df["ISBN"] = df["ISBN"].apply(get_isbn_numeric_id)
 
     return df
+
+
+def make_book_rating_dataset(
+    df_books: pd.DataFrame, df_ratings: pd.DataFrame
+) -> pd.DataFrame:
+    return pd.merge(df_ratings, df_books, on="ISBN")
+
+
+def filter_books_by_ratings(
+    df_book_rating: pd.DataFrame, threshold: int = 25
+) -> pd.DataFrame:
+    rating_count = (
+        df_book_rating.groupby(by=["Book-Title"])["Book-Rating"]
+        .count()
+        .reset_index()
+        .rename(columns={"Book-Rating": "RatingCount_book"})[
+            ["Book-Title", "RatingCount_book"]
+        ]
+    )
+
+    rating_count = rating_count.query("RatingCount_book >= @threshold")
+
+    return rating_count
+
+
+def get_user_rating(rating_count: pd.DataFrame, book_rating: pd.DataFrame):
+    return pd.merge(
+        rating_count,
+        book_rating,
+        left_on="Book-Title",
+        right_on="Book-Title",
+        how="left",
+    )
+
+
+def filter_users_by_ratings(
+    df_user_rating: pd.DataFrame, threshold: int = 20
+) -> pd.DataFrame:
+    user_count = (
+        df_user_rating.groupby(by=["User-ID"])["Book-Rating"]
+        .count()
+        .reset_index()
+        .rename(columns={"Book-Rating": "RatingCount_user"})[
+            ["User-ID", "RatingCount_user"]
+        ]
+    )
+
+    user_count = user_count.query("RatingCount_user >= @threshold")
+
+    return user_count
+
+
+def combine(user_rating: pd.DataFrame, user_count: pd.DataFrame):
+    return user_rating.merge(
+        user_count, left_on="User-ID", right_on="User-ID", how="inner"
+    )
